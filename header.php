20949356828ec372bdd0ec95cfd62bd54ef99461
munenepeter <peternjega7@gmail.com>
2024-02-15 17:12:53 +0300

    fix: update path to css file

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>/style.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
 
     <?php wp_head(); ?>
 </head>
